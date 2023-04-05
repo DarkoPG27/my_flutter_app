cd4cab08e36ed64d8fc39c57252f2bc2808c51d4
DarkoPG27 <darkobrnovic27@gmail.com>
2023-04-05 14:04:30 +0200

    Deleting Transactions and using IconButtons

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import '../models/transaction.dart';
 import 'package:intl/intl.dart';
-import 'package:intl/intl_browser.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
+  final Function deleteTx;
 
-  TransactionList(this.transactions);
+  TransactionList(this.transactions, this.deleteTx);
 
   @override
   Widget build(BuildContext context) {
@@ -55,6 +55,11 @@ class TransactionList extends StatelessWidget {
                     ),
                     subtitle: Text(
                       DateFormat.yMMMd().format(transactions[index].date),
+                    ),
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete),
+                      color: Theme.of(context).errorColor,
+                      onPressed: () => deleteTx(transactions[index].id),
                     ),
                   ),
                 );
