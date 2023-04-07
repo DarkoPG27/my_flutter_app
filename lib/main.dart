8f77538804fc4ded2b2641928bab2f0be9af0f06
DarkoPG27 <darkobrnovic27@gmail.com>
2023-04-07 09:30:41 +0200

    more builder methods

--- lib/main.dart
+++ lib/main.dart
@@ -179,13 +179,8 @@ class _MyHomePageState extends State<MyHomePage> {
     ];
   }
 
-  @override
-  Widget build(BuildContext context) {
-    final mediaQuery = MediaQuery.of(context);
-
-    final isLandscape = mediaQuery.orientation == Orientation.landscape;
-
-    final PreferredSizeWidget appBar = !kIsWeb && Platform.isIOS
+  Widget _buildAppBar() {
+    return !kIsWeb && Platform.isIOS
         ? CupertinoNavigationBar(
             middle: Text(
               'Personal Expenses',
@@ -211,6 +206,15 @@ class _MyHomePageState extends State<MyHomePage> {
               )
             ],
           );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
+
+    final isLandscape = mediaQuery.orientation == Orientation.landscape;
+
+    final PreferredSizeWidget appBar = _buildAppBar();
 
     final txListWidget = Container(
       height: (mediaQuery.size.height -
